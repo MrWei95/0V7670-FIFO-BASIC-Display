eafe4ffeede325d8874d85f47834667e0fde21ba
MrWei95 <149634275+MrWei95@users.noreply.github.com>
2024-07-04 11:31:23 +0800

    Add files via upload

--- Hardware/OV7670/SCCB.c
+++ Hardware/OV7670/SCCB.c
@@ -1,5 +1,5 @@
 #include "sys.h"
-#include "sccb.h"
+#include "SCCB.h"
 #include "delay.h"
 
 void SCCB_Init(void)
@@ -26,12 +26,12 @@ void SCCB_Init(void)
 
 void SCCB_Start(void)
 {
-	SCCB_SDA=1;     //数据线高电平	   
-	SCCB_SCL=1;	    //在时钟线高的时候数据线由高至低
+	SCCB_SDA=1;				//数据线高电平	   
+	SCCB_SCL=1;				//在时钟线高的时候数据线由高至低
 	delay_us(50);  
 	SCCB_SDA=0;
 	delay_us(50);	 
-	SCCB_SCL=0;	    //数据线恢复低电平，单操作函数必要	  
+	SCCB_SCL=0;				//数据线恢复低电平，单操作函数必要	  
 }
 
 void SCCB_Stop(void)
@@ -59,7 +59,7 @@ void SCCB_No_Ack(void)
 u8 SCCB_WR_Byte(u8 dat)
 {
 	u8 j,res;	 
-	for(j=0;j<8;j++) //循环8次发送数据
+	for(j=0;j<8;j++)		//循环8次发送数据
 	{
 		if(dat&0x80)SCCB_SDA=1;	
 		else SCCB_SDA=0;
@@ -69,22 +69,22 @@ u8 SCCB_WR_Byte(u8 dat)
 		delay_us(50);
 		SCCB_SCL=0;		   
 	}			 
-	SCCB_SDA_IN();		//设置SDA为输入 
+	SCCB_SDA_IN();			//设置SDA为输入 
 	delay_us(50);
-	SCCB_SCL=1;			//接收第九位,以判断是否发送成功
+	SCCB_SCL=1;				//接收第九位,以判断是否发送成功
 	delay_us(50);
-	if(SCCB_READ_SDA)res=1;  //SDA=1发送失败，返回1
-	else res=0;         //SDA=0发送成功，返回0
+	if(SCCB_READ_SDA)res=1;	//SDA=1发送失败，返回1
+	else res=0;				//SDA=0发送成功，返回0
 	SCCB_SCL=0;		 
-	SCCB_SDA_OUT();		//设置SDA为输出    
+	SCCB_SDA_OUT();			//设置SDA为输出    
 	return res;  
 }	 
 
 u8 SCCB_RD_Byte(void)
 {
 	u8 temp=0,j;    
-	SCCB_SDA_IN();		//设置SDA为输入  
-	for(j=8;j>0;j--) 	//循环8次接收数据
+	SCCB_SDA_IN();			//设置SDA为输入  
+	for(j=8;j>0;j--)		//循环8次接收数据
 	{		     	  
 		delay_us(50);
 		SCCB_SCL=1;
@@ -93,19 +93,19 @@ u8 SCCB_RD_Byte(void)
 		delay_us(50);
 		SCCB_SCL=0;
 	}	
-	SCCB_SDA_OUT();		//设置SDA为输出    
+	SCCB_SDA_OUT();			//设置SDA为输出    
 	return temp;
 } 							    
 
 u8 SCCB_WR_Reg(u8 reg,u8 data)
 {
 	u8 res=0;
-	SCCB_Start(); 					//启动SCCB传输
+	SCCB_Start();			//启动SCCB传输
 	if(SCCB_WR_Byte(SCCB_ID))res=1;	//写器件ID	  
 	delay_us(100);
 	if(SCCB_WR_Byte(reg))res=1;		//写寄存器地址	  
 	delay_us(100);
-	if(SCCB_WR_Byte(data))res=1; 	//写数据	 
+	if(SCCB_WR_Byte(data))res=1;	//写数据	 
 	SCCB_Stop();	  
 	return	res;
 }		  					    
@@ -113,10 +113,10 @@ u8 SCCB_WR_Reg(u8 reg,u8 data)
 u8 SCCB_RD_Reg(u8 reg)
 {
 	u8 val=0;
-	SCCB_Start(); 				//启动SCCB传输
-	SCCB_WR_Byte(SCCB_ID);		//写器件ID	  
+	SCCB_Start();			//启动SCCB传输
+	SCCB_WR_Byte(SCCB_ID);	//写器件ID	  
 	delay_us(100);	 
-  SCCB_WR_Byte(reg);			//写寄存器地址	  
+  SCCB_WR_Byte(reg);		//写寄存器地址	  
 	delay_us(100);	  
 	SCCB_Stop();   
 	delay_us(100);	   
@@ -124,7 +124,7 @@ u8 SCCB_RD_Reg(u8 reg)
 	SCCB_Start();
 	SCCB_WR_Byte(SCCB_ID|0X01);	//发送读命令	  
 	delay_us(100);
-	val=SCCB_RD_Byte();		 	//读取数据
+	val=SCCB_RD_Byte();		//读取数据
 	SCCB_No_Ack();
 	SCCB_Stop();
 	return val;
---
+++ Hardware/OV7670/SCCB.h
@@ -0,0 +1,28 @@
+#ifndef __SCCB_H
+#define __SCCB_H
+#include "sys.h"
+
+#define SCCB_SCL_PORT    		GPIOA
+#define SCCB_SDA_PORT    		GPIOA
+#define SCCB_SCL_PIN    		GPIO_Pin_0		//PA0连接SCL
+#define SCCB_SDA_PIN    		GPIO_Pin_1		//PA1连接SDA
+
+#define SCCB_SDA_IN()			{GPIOA->CRL &= 0XFFFFFF0F;GPIOA->CRL |= 0X00000080;}
+#define SCCB_SDA_OUT()			{GPIOA->CRL &= 0XFFFFFF0F;GPIOA->CRL |= 0X00000030;}
+
+#define SCCB_SCL				PAout(0)
+#define SCCB_SDA				PAout(1)
+
+#define SCCB_READ_SDA			PAin(1)
+#define SCCB_ID					0X42
+
+void SCCB_Init(void);
+void SCCB_Start(void);
+void SCCB_Stop(void);
+void SCCB_No_Ack(void);
+u8 SCCB_WR_Byte(u8 dat);
+u8 SCCB_RD_Byte(void);
+u8 SCCB_WR_Reg(u8 reg,u8 data);
+u8 SCCB_RD_Reg(u8 reg);
+
+#endif
